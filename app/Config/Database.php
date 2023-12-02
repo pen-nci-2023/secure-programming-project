355dea99c9482a52c62e2ee068d7a427a744d459
Penuel Maypa (x16382003) <x16382003@student.ncirl.ie>
2023-12-02 08:26:59 +0000

    Im scrapping , starting fron the start

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -30,7 +30,7 @@ class Database extends Config
         'username'     => '',
         'password'     => '',
         'database'     =>  APPPATH . 'database/mydatabase.sqlite',
-        'DBDriver'     => 'pdo',
+        'DBDriver'     => 'SQLite3',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
